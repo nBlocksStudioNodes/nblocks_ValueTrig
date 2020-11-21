a8909f8ada8913995ba0bac032a80b69dfa07a2e
nikos chalikias <nikos.chalikias@gmail.com>
2020-11-21 23:20:01 +0000

    renamed all references from counter to valuetrig

--- valuetrig.cpp
+++ valuetrig.cpp
@@ -1,15 +1,15 @@
-#include "counter.h"
+#include "valuetrig.h"
 
-nBlock_Counter::nBlock_Counter(uint32_t const_value){
+nBlock_ValueTrig::nBlock_ValueTrig(uint32_t const_value){
 	_val = const_value;
 	return;
 }
 
-void nBlock_Counter::triggerInput(nBlocks_Message message){			
+void nBlock_ValueTrig::triggerInput(nBlocks_Message message){			
 		output[0] = _val;
 		available[0] = 1;	
 }
 
-void nBlock_Counter::endFrame(void) {
+void nBlock_ValueTrig::endFrame(void) {
 
 }
--- valuetrig.h
+++ valuetrig.h
@@ -1,12 +1,12 @@
-#ifndef __NB_COUNTER
-#define __NB_COUNTER
+#ifndef __NB_VALUETRIG
+#define __NB_VALUETRIG
 
 #include "mbed.h"
 #include "nworkbench.h"
 
-class nBlock_Counter: public nBlockSimpleNode<1> {
+class nBlock_ValueTrig: public nBlockSimpleNode<1> {
 public:
-    nBlock_Counter(uint32_t const_value);
+    nBlock_ValueTrig(uint32_t const_value);
     void triggerInput(nBlocks_Message message);
 	void endFrame(void);
    
